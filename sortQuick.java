884a6faa2db3dafafb19e0d4a1a70814765c1b42
Marcus <TiplerMF@cardiff.ac.uk>
2025-04-07 00:30:11 +0100

    none working version to be updated

--- sortQuick.java
+++ sortQuick.java
@@ -2,9 +2,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class sortQuick {
+
     public static List<Integer> sorterQuick(List<Integer> listItems) {
         List<Integer> sortedList = new ArrayList<>(listItems);
-        int pivot = listItems.get(0);
+        int pivot = sortedList.get(0);
+        int low = - 1;
+        int high = listItems.size();
+        if (low < high) {
+            List<Integer> pi = hoarePartition(sortedList);
+            sorterQuick(pi);
+        }
+        return sortedList;
+    }
+
+
+    public static List<Integer> hoarePartition(List<Integer> listItems) {
+        List<Integer> sortedList = new ArrayList<>(listItems);
+        int pivot = sortedList.get(0);
         int low = - 1;
         int high = listItems.size();
         while (true) { 
@@ -26,7 +40,7 @@ public class sortQuick {
     }
 
 
-    public static List<Integer> sorterReverseQuick(List<Integer> listItems) {
+    public static List<Integer> hoarePartitionReverse(List<Integer> listItems) {
         List<Integer> reversedList = new ArrayList<>(listItems);
         int pivot = listItems.get(0);
         int low = - 1;
@@ -49,16 +63,21 @@ public class sortQuick {
         }
     }
 
+    public static int getRandomNumbers(int max) {
+        return (int) ((Math.random() * (max - 1)) + 1);
+    }
+
 
     public static void main(String[] args) {
         List<Integer> amountItems = new ArrayList<Integer>();
         for (int i = 1; i < 8; i++){
-            amountItems.add(i);
+            amountItems.add(getRandomNumbers(8));
         }
 
         List<Integer> sortedList = sorterQuick(amountItems);
-        List<Integer> sortedBackwards = sorterReverseQuick(amountItems);
-        System.out.println(sortedList);
-        System.out.println(sortedBackwards);
+        // List<Integer> sortedBackwards = sorterReverseQuick(amountItems);
+        System.out.println("Un-sorted list: " + amountItems);
+        System.out.println("Sorted list: " + sortedList);
+        // System.out.println("Reversed list: " + sortedBackwards);
     }
 }
