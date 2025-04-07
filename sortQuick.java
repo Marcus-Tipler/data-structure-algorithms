4f05db26578f141b115a2daa2bb053e66e9bda4c
Marcus <TiplerMF@cardiff.ac.uk>
2025-04-07 06:43:34 +0100

    Working sort algorithm with fixes.

--- sortQuick.java
+++ sortQuick.java
@@ -4,40 +4,57 @@ import java.util.List;
 public class sortQuick {
 
     public static List<Integer> sorterQuick(List<Integer> listItems) {
-        List<Integer> sortedList = new ArrayList<>(listItems);
-        int pivot = sortedList.get(0);
-        int low = - 1;
-        int high = listItems.size();
-        if (low < high) {
-            List<Integer> pi = hoarePartition(sortedList);
-            sorterQuick(pi);
+        List<Integer> database = new ArrayList<>(listItems);
+        if (database.size() < 2) {
+			return database;
+		}
+        else {
+            int leftBound = 0;
+            int rightBound = database.size() - 1;
+            hoarePartition(database, leftBound, rightBound);
+            return database;
         }
-        return sortedList;
     }
 
 
-    public static List<Integer> hoarePartition(List<Integer> listItems) {
-        List<Integer> sortedList = new ArrayList<>(listItems);
-        int pivot = sortedList.get(0);
-        int low = - 1;
-        int high = listItems.size();
-        while (true) { 
-            low++;
-            while (listItems.get(low) > pivot) {
-                low++;
-            }
-            high--;
-            while (listItems.get(high) > pivot) {
-                high--;
-            }
-            if (low >= high) {
-                return sortedList;
-            }
-            int temp = listItems.get(low);
-            sortedList.set(low, listItems.get(high));
-            sortedList.set(high, temp);
-        }
-    }
+    public static void hoarePartition(List<Integer> database, int leftBound, int rightBound) {
+
+        if (leftBound >= rightBound)
+			return; // indices have crossed;
+
+		int pivot = database.get(rightBound);
+		int leftInd = leftBound; // will scan leftward
+		int rightInd = rightBound - 1;// will scan right until lager than the pivot
+
+
+		while (leftInd <= rightInd) { // scan right until larger than the pivot
+
+			while ((leftInd <= rightInd) && (database.get(leftInd) >= pivot)) {
+				leftInd++;
+			}
+
+			while ((rightInd >= leftInd) && (database.get(rightInd) <= pivot)) {
+				rightInd--;
+			}
+
+			if (leftInd < rightInd) { // both elements were found so swap
+
+				Swap(database, leftInd, rightInd);
+			}
+
+		} // loop continues until indices cross
+
+		Swap(database, rightBound, leftInd);
+
+		hoarePartition(database, leftBound, leftInd - 1); // left recursive call
+		hoarePartition(database, leftInd + 1, rightBound); // right recursive call
+	}
+
+    public static void Swap(List<Integer> database, int j, int i) {
+		int temp = database.get(j);
+		database.set(j, database.get(i));
+		database.set(i, temp);
+	}
 
 
     public static List<Integer> hoarePartitionReverse(List<Integer> listItems) {
@@ -47,11 +64,11 @@ public class sortQuick {
         int high = listItems.size();
         while (true) { 
             low++;
-            while (listItems.get(low) < pivot) {
+            while (listItems.get(low) > pivot) {
                 low++;
             }
             high--;
-            while (listItems.get(high) < pivot) {
+            while (listItems.get(high) > pivot) {
                 high--;
             }
             if (low >= high) {
