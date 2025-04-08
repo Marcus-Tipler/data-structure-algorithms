3da4e26003eba645995252199ecbea4ff36ce4e1
Marcus <TiplerMF@cardiff.ac.uk>
2025-04-08 04:54:34 +0100

    backup

--- algorithmEfficiency.java
+++ algorithmEfficiency.java
@@ -1,5 +1,3 @@
-import java.time.Clock;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,33 +34,63 @@ public class algorithmEfficiency extends sortQuick {
         algorithmEfficiency aE = new algorithmEfficiency();
 
         // Variables for array sizes per day.
-        int[] amountItems = {1000, 5000, 10000, 50000, 75000, 100000, 500000};
+        int[] amountItems = {1000, 2000, 3000, 4000, 5000, 6000, 500000};
         
         // Initializing variables for the sorted and reversed lists.
-        List<Integer> week;
-        List<Integer> weekQuickNormal, weekQuickReverse, weekQuickSorted;
+        List<Integer> week, weekQuickNormal, weekQuickReverse, weekQuickSorted;
         long clockStart, clockNormal, clockReverse, clockSorted;
 
+        ArrayList<List<Integer>> weeks = new ArrayList<List<Integer>>();
+        ArrayList<List<Integer>> weeksSorted = new ArrayList<List<Integer>>();
         // Start timing the Normal Quick Sort Method.
-        for (int i = 0; i < amountItems.length; i++) {                  // FIXME: Problem, this needs to be isolated and done on an individual basis.
+        for (int i = 0; i < amountItems.length; i++) { 
             week = aE.createArray(amountItems[i]);
-            System.out.println("NORMAL");
-            clockStart = System.currentTimeMillis();
-            weekQuickNormal = sorterQuick(week);
-            clockNormal = System.currentTimeMillis();
-            long deltaNormal = aE.calculateTime(clockStart, clockNormal);
-            System.out.println("REVERSED");
-            weekQuickReverse = sorterQuickReversed(week);
-            clockReverse = System.currentTimeMillis();
-            long deltaReverse = aE.calculateTime(clockNormal, clockReverse);
-            System.out.println("SORTED");
-            weekQuickSorted = quickSort(weekQuickNormal, weekQuickNormal.size());          // FIXME: Problem with sorted array causes Crash.
-            clockSorted = System.currentTimeMillis();                // FIXME: Problem with sorted array causes Crash.
-            long deltaSorted = aE.calculateTime(clockReverse, clockSorted);
-            // System.out.println(week);                                // TODO: Test Line.
-            // System.out.println(weekSorted);                          // TODO: Test Line.
+            weeks.add(week); 
         }
-        System.out.println("\n\n");
+        // System.out.println(weeks);
+
+        clockStart = System.currentTimeMillis();
+        for (int i = 0; i < weeks.size(); i++) {
+            week = weeks.get(i);
+            weekQuickNormal = quickSort(week, week.size());
+            weeksSorted.add(weekQuickNormal);
+        }
+        clockNormal = System.currentTimeMillis();
+        System.out.println(clockNormal - clockStart);
+        for (int i = 0; i < weeks.size(); i++) {
+            week = weeks.get(i);
+            weekQuickReverse = quickSortReverse(week, week.size());
+        }
+        clockReverse = System.currentTimeMillis();
+        System.out.println(clockReverse - clockNormal);
+        for (int i = 0; i < weeksSorted.size(); i++) {
+            week = weeksSorted.get(i);
+            weekQuickSorted = quickSort(week, week.size());
+        }
+        clockSorted = System.currentTimeMillis();
+        System.out.println(clockSorted - clockReverse);
+        // System.out.println(weeks);
+        // for (int i = 0; i < amountItems.length; i++) {                  // FIXME: Problem, this needs to be isolated and done on an individual basis.
+        //     week = aE.createArray(amountItems[i]);
+        //     System.out.println("NORMAL");
+        //     clockStart = System.currentTimeMillis();
+        //     weekQuickNormal = quickSort(week, week.size());
+        //     clockNormal = System.currentTimeMillis();
+        //     long deltaNormal = aE.calculateTime(clockStart, clockNormal);
+        //     System.out.println("REVERSED");
+        //     weekQuickReverse = quickSortReverse(week, week.size());
+        //     clockReverse = System.currentTimeMillis();
+        //     long deltaReverse = aE.calculateTime(clockNormal, clockReverse);
+        //     System.out.println("SORTED");
+        //     weekQuickSorted = quickSort(weekQuickNormal, weekQuickNormal.size());          // FIXME: Problem with sorted array causes Crash.
+        //     clockSorted = System.currentTimeMillis();                // FIXME: Problem with sorted array causes Crash.
+        //     long deltaSorted = aE.calculateTime(clockReverse, clockSorted);
+        //     System.out.println(week);                                // TODO: Test Line.
+        //     System.out.println(weekQuickNormal);                          // TODO: Test Line.
+        //     System.out.println(weekQuickReverse);                          // TODO: Test Line.
+        //     System.out.println(weekQuickSorted);                          // TODO: Test Line.
+        // }
+        // System.out.println("\n\n");
 
 
         // long startTime = System.currentTimeMillis();
--- sortQuick.java
+++ sortQuick.java
@@ -5,128 +5,17 @@ public class sortQuick {
     // ----------------------------------------------------------------
     // Declares values for quick sort and reversed quick sort methods.
     // ----------------------------------------------------------------
-    public static List<Integer> sorterQuick(List<Integer> listItems) {
-        List<Integer> database = new ArrayList<>(listItems);
-        if (database.size() < 2) {
-			return database;
-		}
-        else {
-            int leftBound = 0;
-            int rightBound = database.size() - 1;
-            hoarePartition(database, leftBound, rightBound);
-            return database;
-        }
-    }
-    public static List<Integer> sorterQuickReversed(List<Integer> listItems) {
-        List<Integer> database = new ArrayList<>(listItems);
-        if (database.size() < 2) {
-			return database;
-		}
-        else {
-            int leftBound = 0;
-            int rightBound = database.size() - 1;
-            hoarePartitionReverse(database, leftBound, rightBound);
-            return database;
-        }
-    }
-
-
-    // ----------------------------------------------------------------
-    // Hoare partition method for quick sort (has two sides).
-    // This has both methods for reverse and normal quick sort.
-    // ----------------------------------------------------------------
-    public static void hoarePartition(List<Integer> database, int leftBound, int rightBound) {
-        if (leftBound >= rightBound)
-			return; // indices have crossed;
-		int pivot = database.get(rightBound);
-		int leftInd = leftBound; // will scan leftward
-		int rightInd = rightBound - 1;// will scan right until lager than the pivot
-		while (leftInd <= rightInd) { // scan right until larger than the pivot
-			while ((leftInd <= rightInd) && (database.get(leftInd) <= pivot)) {
-				leftInd++;
-			}
-			while ((rightInd >= leftInd) && (database.get(rightInd) >= pivot)) {
-				rightInd--;
-			}
-			if (leftInd < rightInd) { // both elements were found so swap
-				Swap(database, leftInd, rightInd);
-			}
-		} // loop continues until indices cross
-		Swap(database, rightBound, leftInd);
-		hoarePartition(database, leftBound, leftInd - 1); // left recursive call
-		hoarePartition(database, leftInd + 1, rightBound); // right recursive call
-	}
-    public static void hoarePartitionReverse(List<Integer> database, int leftBound, int rightBound) {
-        if (leftBound >= rightBound)
-			return; // indices have crossed;
-		int pivot = database.get(rightBound);
-		int leftInd = leftBound; // will scan leftward
-		int rightInd = rightBound - 1;// will scan right until lager than the pivot
-		while (leftInd <= rightInd) { // scan right until larger than the pivot
-			while ((leftInd <= rightInd) && (database.get(leftInd) >= pivot)) {
-				leftInd++;
-			}
-			while ((rightInd >= leftInd) && (database.get(rightInd) <= pivot)) {
-				rightInd--;
-			}
-			if (leftInd < rightInd) { // both elements were found so swap
-				Swap(database, leftInd, rightInd);
-			}
-		} // loop continues until indices cross
-		Swap(database, rightBound, leftInd);
-		hoarePartitionReverse(database, leftBound, leftInd - 1); // left recursive call
-		hoarePartitionReverse(database, leftInd + 1, rightBound); // right recursive call
-	}
-
-
-    // ----------------------------------------------------------------
-    // Method for swapping a partition in the array for either sort methods.
-    // -----------------------------------------------------------------
-    public static void Swap(List<Integer> database, int j, int i) {
-		int temp = database.get(j);
-		database.set(j, database.get(i));
-		database.set(i, temp);
-	}
-
-    public static int getRandomNumbers(int max) {
-        return (int) ((Math.random() * (max - 1)) + 1);
-    }
-
-
-    // ----------------------------------------------------------------
-    // Main method for testing the quick sort algorithm outside the class.
-    // -----------------------------------------------------------------
-    public static void main(String[] args) {
-        List<Integer> amountItems = new ArrayList<Integer>();
-        for (int i = 1; i < 8; i++){
-            // amountItems.add(getRandomNumbers(8));
-            amountItems.add(1);
-        }
-        List<Integer> sortedList = sorterQuick(amountItems);
-        List<Integer> sortedBackwards = sorterQuickReversed(amountItems);
-        System.out.println("Un-sorted list: " + amountItems);
-        System.out.println("Sorted list: " + sortedList);
-        System.out.println("Reversed list: " + sortedBackwards);
-
-        quickSort(amountItems, amountItems.size());
-    }
-
-
-
-
-    // ----------------------------------------------------------------
-    // FIXME: TESTING ONLY
-    // ----------------------------------------------------------------
-    public static List<Integer> quickSort(List<Integer> arr, int elements) {
-        int i=0,L,R,pivot;
-        int[] beg = new int[10000], end = new int[10000]; // FIXME: 10k needs replacing in the longer term after further testing is complete. FOR BOTH.
+    public static List<Integer> quickSort(List<Integer> Array, int elements) {
+        List<Integer> arr = new ArrayList<>(Array);
+        int i=0,L,R,pivot, arraySize = arr.size() + 1;
+        int[] beg = new int[arraySize], end = new int[arraySize];
         beg[0]=0;
         end[0]=elements;
         while(i>=0) {
             L=beg[i];
             R=end[i]-1;
             if(L<R) {
-                pivot=arr.get(L); if(i==10000-1) return arr; // FIXME: 10k needs replacing in the longer term after further testing is complete. (false replaced)
+                pivot=arr.get(L); if(i==arraySize-1) return arr;
                 while(L<R) {
                     while(arr.get(R) >= pivot && L < R ) R--; if(L < R) arr.set(L++, arr.get(R));
                     while(arr.get(L) <= pivot && L < R) L++; if(L < R) arr.set(R--, arr.get(L));
@@ -140,5 +29,50 @@ public class sortQuick {
             }
         }
         return arr;
+    }
+
+    public static List<Integer> quickSortReverse(List<Integer> Array, int elements) {
+        List<Integer> arr = new ArrayList<>(Array);
+        int i=0,L,R,pivot, arraySize = arr.size() + 1;
+        int[] beg = new int[arraySize], end = new int[arraySize];
+        beg[0]=0;
+        end[0]=elements;
+        while(i>=0) {
+            L=beg[i];
+            R=end[i]-1;
+            if(L<R) {
+                pivot=arr.get(L); if(i==arraySize-1) return arr;
+                while(L<R) {
+                    while(arr.get(R) <= pivot && L < R ) R--; if(L < R) arr.set(L++, arr.get(R));
+                    while(arr.get(L) >= pivot && L < R) L++; if(L < R) arr.set(R--, arr.get(L));
+                }
+                arr.set(L, pivot);
+                beg[i+1]=L+1;
+                end[i+1]=end[i];
+                end[i++]=L;
+            } else {
+                i--;
+            }
+        }
+        return arr;
+    }
+
+
+    // ----------------------------------------------------------------
+    // Main method for testing the quick sort algorithm outside the class.
+    // -----------------------------------------------------------------
+    public static void main(String[] args) {
+        List<Integer> amountItems = new ArrayList<Integer>();
+        for (int i = 1; i < 8; i++){
+            // amountItems.add(getRandomNumbers(8));
+            amountItems.add(1);
+        }
+        List<Integer> sortedList = quickSort(amountItems, amountItems.size());
+        List<Integer> sortedBackwards = quickSortReverse(amountItems, amountItems.size());
+        System.out.println("Un-sorted list: " + amountItems);
+        System.out.println("Sorted list: " + sortedList);
+        System.out.println("Reversed list: " + sortedBackwards);
+
+        quickSort(amountItems, amountItems.size());
     }
 }
