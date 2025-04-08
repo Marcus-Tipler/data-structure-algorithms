dc44c0b3e125ef958c420e664e7c647f868cba20
Marcus <TiplerMF@cardiff.ac.uk>
2025-04-08 23:33:00 +0100

    Dreadful debugging nightmare over, testing completed and sorted array doesn't take more than 8ms to complete.

--- algorithmEfficiency.java
+++ algorithmEfficiency.java
@@ -40,37 +40,55 @@ public class algorithmEfficiency extends sortQuick {
         List<Integer> week, weekQuickNormal, weekQuickReverse, weekQuickSorted;
         long clockStart, clockNormal, clockReverse, clockSorted;
 
-        ArrayList<List<Integer>> weeks = new ArrayList<List<Integer>>();
-        ArrayList<List<Integer>> weeksSorted = new ArrayList<List<Integer>>();
+        ArrayList<List<Integer>> weeks = new ArrayList<>();
+        ArrayList<List<Integer>> weeksSortedNormal = new ArrayList<>(), weeksSortedReverse = new ArrayList<>(), weeksSortedSorted = new ArrayList<>();
         // Start timing the Normal Quick Sort Method.
         for (int i = 0; i < amountItems.length; i++) { 
             week = aE.createArray(amountItems[i]);
             weeks.add(week); 
         }
         // System.out.println(weeks);
-
+        int[] weekQuickNormy, weekQuickReversy;
         clockStart = System.currentTimeMillis();
         for (int i = 0; i < weeks.size(); i++) {
             week = weeks.get(i);
-            weekQuickNormal = quickSort(week, week.size());
-            weeksSorted.add(weekQuickNormal);
+            weekQuickReverse = new ArrayList<>(week);
+            // weekQuickNormal = quickSort(week, week.size());
+            weekQuickNormy = quickSort(week, week.size());
+            for (i = 0; i < week.size(); i++) {
+                weekQuickReverse.set(i, weekQuickNormy[i]);
+            }
+            weekQuickReversy = quickSort(weekQuickReverse, weekQuickReverse.size());
+            // weeksSortedNormal.add(weekQuickNormal);
+            System.out.println("Unsorted Array: ");
+            for (int element = 0; element < weekQuickNormy.length; element++) {
+                System.out.print(weekQuickNormy[element] + ", ");
+            }
+            System.out.println("\n -------------------------------------------- ");
+            System.out.println("Sorted Array: ");
+            for (int element = 0; element < weekQuickReversy.length; element++) {
+                System.out.print(weekQuickReversy[element] + ", ");
+            }
+            System.out.println("\n -------------------------------------------- ");
         }
         clockNormal = System.currentTimeMillis();
         System.out.println(clockNormal - clockStart);
-        for (int i = 0; i < weeks.size(); i++) {
-            week = weeks.get(i);
-            weekQuickReverse = quickSortReverse(week, week.size());
-        }
-        clockReverse = System.currentTimeMillis();
-        System.out.println(clockReverse - clockNormal);
-        for (int i = 0; i < weeksSorted.size(); i++) {
-            week = weeksSorted.get(i);
-            weekQuickSorted = quickSort(week, week.size());
-        }
-        clockSorted = System.currentTimeMillis();
-        System.out.println(clockSorted - clockReverse);
+        // for (int i = 0; i < weeks.size(); i++) {
+        //     week = weeks.get(i);
+        //     weekQuickReverse = quickSortReverse(week, week.size());
+        //     weeksSortedReverse.add(weekQuickReverse);
+        // }
+        // clockReverse = System.currentTimeMillis();
+        // System.out.println(clockReverse - clockNormal);
+        // for (int i = 0; i < weeksSortedNormal.size(); i++) {
+        //     week = weeksSortedNormal.get(i);
+        //     weekQuickSorted = quickSort(week, week.size());
+        //     weeksSortedSorted.add(weekQuickSorted);
+        // }
+        // clockSorted = System.currentTimeMillis();
+        // System.out.println(clockSorted - clockReverse);
         // System.out.println(weeks);
-        // for (int i = 0; i < amountItems.length; i++) {                  // FIXME: Problem, this needs to be isolated and done on an individual basis.
+        // for (int i = 0; i < amountItems.length; i++) {                                       // FIXME: Problem, this needs to be isolated and done on an individual basis.
         //     week = aE.createArray(amountItems[i]);
         //     System.out.println("NORMAL");
         //     clockStart = System.currentTimeMillis();
@@ -82,13 +100,13 @@ public class algorithmEfficiency extends sortQuick {
         //     clockReverse = System.currentTimeMillis();
         //     long deltaReverse = aE.calculateTime(clockNormal, clockReverse);
         //     System.out.println("SORTED");
-        //     weekQuickSorted = quickSort(weekQuickNormal, weekQuickNormal.size());          // FIXME: Problem with sorted array causes Crash.
-        //     clockSorted = System.currentTimeMillis();                // FIXME: Problem with sorted array causes Crash.
+        //     weekQuickSorted = quickSort(weekQuickNormal, weekQuickNormal.size());            // FIXME: Problem with sorted array causes Crash.
+        //     clockSorted = System.currentTimeMillis();                                        // FIXME: Problem with sorted array causes Crash.
         //     long deltaSorted = aE.calculateTime(clockReverse, clockSorted);
-        //     System.out.println(week);                                // TODO: Test Line.
-        //     System.out.println(weekQuickNormal);                          // TODO: Test Line.
-        //     System.out.println(weekQuickReverse);                          // TODO: Test Line.
-        //     System.out.println(weekQuickSorted);                          // TODO: Test Line.
+        //     System.out.println(week);                                                        // TODO: Test Line.
+        //     System.out.println(weekQuickNormal);                                             // TODO: Test Line.
+        //     System.out.println(weekQuickReverse);                                            // TODO: Test Line.
+        //     System.out.println(weekQuickSorted);                                             // TODO: Test Line.
         // }
         // System.out.println("\n\n");
 
--- sortQuick.java
+++ sortQuick.java
@@ -1,77 +1,79 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class sortQuick {
     // ----------------------------------------------------------------
     // Declares values for quick sort and reversed quick sort methods.
     // ----------------------------------------------------------------
-    public static List<Integer> quickSort(List<Integer> Array, int elements) {
-        List<Integer> arr = new ArrayList<>(Array);
-        int i=0,L,R,pivot, arraySize = arr.size() + 1;
-        int[] beg = new int[arraySize], end = new int[arraySize];
-        beg[0]=0;
-        end[0]=elements;
-        while(i>=0) {
-            L=beg[i];
-            R=end[i]-1;
-            if(L<R) {
-                pivot=arr.get(L); if(i==arraySize-1) return arr;
-                while(L<R) {
-                    while(arr.get(R) >= pivot && L < R ) R--; if(L < R) arr.set(L++, arr.get(R));
-                    while(arr.get(L) <= pivot && L < R) L++; if(L < R) arr.set(R--, arr.get(L));
-                }
-                arr.set(L, pivot);
-                beg[i+1]=L+1;
-                end[i+1]=end[i];
-                end[i++]=L;
-            } else {
-                i--;
+    public static int[] quickSort(List<Integer> Array, int elements) {
+        int[] arr = new int[elements];
+        for (int element = 0; element < elements; element++) {
+            arr[element] = Array.get(element);
+        }
+        if(arr.length <= 0) return arr;
+        Stack<Integer> stack = new Stack<Integer>();
+
+        stack.push(0);
+        stack.push(arr.length - 1);
+        while(!stack.isEmpty()){
+            int high = stack.pop();
+            int low = stack.pop();
+
+            int pivotIdx = partition(arr, low, high);
+            if(pivotIdx > low) {
+                stack.push(low);
+                stack.push(pivotIdx - 1);
+            }
+            if(pivotIdx < high && pivotIdx >= 0){
+                stack.push(pivotIdx + 1);
+                stack.push(high);
             }
         }
         return arr;
     }
 
-    public static List<Integer> quickSortReverse(List<Integer> Array, int elements) {
-        List<Integer> arr = new ArrayList<>(Array);
-        int i=0,L,R,pivot, arraySize = arr.size() + 1;
-        int[] beg = new int[arraySize], end = new int[arraySize];
-        beg[0]=0;
-        end[0]=elements;
-        while(i>=0) {
-            L=beg[i];
-            R=end[i]-1;
-            if(L<R) {
-                pivot=arr.get(L); if(i==arraySize-1) return arr;
-                while(L<R) {
-                    while(arr.get(R) <= pivot && L < R ) R--; if(L < R) arr.set(L++, arr.get(R));
-                    while(arr.get(L) >= pivot && L < R) L++; if(L < R) arr.set(R--, arr.get(L));
-                }
-                arr.set(L, pivot);
-                beg[i+1]=L+1;
-                end[i+1]=end[i];
-                end[i++]=L;
-            } else {
-                i--;
-            }
-        }
-        return arr;
-    }
+    private static int partition(int[] arr, int low, int high){
+        if(arr.length <= 0) return -1;
+        if(low >= high) return -1;
+        int l = low;
+        int r = high;
 
+        int pivot = arr[l];
+        while(l < r){
+            while(l < r && arr[r] >= pivot){ 
+                r--;
+            }
+            arr[l] = arr[r];
+            while(l < r && arr[l] <= pivot){
+                l++;
+            }
+            arr[r] = arr[l];
+        }
+        arr[l] = pivot;
+        return l;
+    }
 
     // ----------------------------------------------------------------
     // Main method for testing the quick sort algorithm outside the class.
     // -----------------------------------------------------------------
     public static void main(String[] args) {
-        List<Integer> amountItems = new ArrayList<Integer>();
+        List<Integer> amountItems = new ArrayList<>();
         for (int i = 1; i < 8; i++){
             // amountItems.add(getRandomNumbers(8));
             amountItems.add(1);
         }
-        List<Integer> sortedList = quickSort(amountItems, amountItems.size());
-        List<Integer> sortedBackwards = quickSortReverse(amountItems, amountItems.size());
+        // List<Integer> sortedList = quickSort(amountItems, amountItems.size());
+        // List<Integer> sortedBackwards = quickSortReverse(amountItems, amountItems.size());
+        int[] sortedList = quickSort(amountItems, amountItems.size());
+        
         System.out.println("Un-sorted list: " + amountItems);
-        System.out.println("Sorted list: " + sortedList);
-        System.out.println("Reversed list: " + sortedBackwards);
+        System.out.print("Sorted list: ");
+        for (int element = 0; element < sortedList.length; element++) {
+            System.out.print(sortedList[element] + " ");
+        }
+        System.out.println("");
+        // System.out.println("Reversed list: " + sortedBackwards);
 
         quickSort(amountItems, amountItems.size());
     }
